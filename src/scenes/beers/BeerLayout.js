37b351e004b37005008dc37bff785fc9be73a3e8
desfero <pavloblack@hotmail.com>
2018-07-24 23:00:18 +0200

    :sparkles: Add more information about beer to modal

--- src/scenes/beers/BeerLayout.js
+++ src/scenes/beers/BeerLayout.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
-const BeerLayout = ({ beer }) => (
-        <h3>{beer.name}</h3>
+const BeerLayout = ({beer}) => (
+    <sectino>
+        <header>
+            <h3>{beer.name}</h3>
+            <p>{beer.tagline}</p>
+        </header>
+        <span>
+            <strong>IBU:</strong>
+            {beer.ibu}
+        </span>
+        <span>
+            <strong>ABV:</strong>
+            {beer.abv}
+        </span>
+        <span>
+            <strong>EBC:</strong>
+            {beer.ebc}
+        </span>
+        <section>
+            <p>{beer.description}</p>
+            <h4>
+                Best served with:
+            </h4>
+            <ul aria-labelledby="servedWithHeading">
+                {beer.food_pairing.map((pairing, i) => <li key={i}>{pairing}</li>)}
+            </ul>
+        </section>
+    </sectino>
 );
 
 const BeerNotFoundLayout = () => 'Beer with specified id not found';
