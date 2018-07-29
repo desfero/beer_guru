887491d47dddb79cafe3aa37fa159af18e77bd0a
desfero <pavloblack@hotmail.com>
2018-07-29 23:04:36 +0300

    :sparkles: Log redux actions / saga errors to sentry

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,23 @@
+import Raven from 'raven-js';
 import {createStore, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
+import createRavenMiddleware from 'raven-for-redux';
 import {rootReducer} from './rootReducer';
 import {rootSaga} from './rootSaga';
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware({
+    onError: e => Raven.captureException(e)
+});
 
 const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(sagaMiddleware))
+    composeWithDevTools(
+        applyMiddleware(
+            sagaMiddleware,
+            createRavenMiddleware(Raven),
+        )
+    ),
 );
 
 sagaMiddleware.run(rootSaga);
