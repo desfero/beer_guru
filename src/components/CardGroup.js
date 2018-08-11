cf8fba32d1048714fc2603dde90c2ace0752b078
desfero <pavloblack@hotmail.com>
2018-08-11 17:48:05 +0200

    :lipstick: Add RWD support for cards

--- src/components/CardGroup.js
+++ src/components/CardGroup.js
@@ -1,9 +1,23 @@
 import styled from 'styled-components';
+import {media} from '../theme/sizes';
 
 const CardGroup = styled.section`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     grid-gap: 25px;
+    
+   
+    ${
+        media.tablet`
+            grid-template-columns: 1fr 1fr;    
+       `
+    }
+    
+    ${
+        media.phone`
+            grid-template-columns: 1fr;    
+        `
+    }  
 `;
 
 export {CardGroup};
