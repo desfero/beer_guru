a5a63a84f2099df3f51062deff1493f179f92c02
Paweł Lula <pavloblack@hotmail.com>
2019-05-29 20:02:31 +0800

    :ambulance: Fix broken storybook build

--- packages/layout/.storybook/config.js
+++ packages/layout/.storybook/config.js
@@ -1,8 +1,18 @@
 import { addDecorator, configure } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs/react';
 import { checkA11y } from '@storybook/addon-a11y';
+import { withThemes } from 'storybook-styled-components';
+import StoryRouter from 'storybook-react-router';
+
+import { theme } from '../src'
+
+const themes = {
+  'Default': theme,
+};
 
 addDecorator(withKnobs);
+addDecorator(withThemes(themes));
+addDecorator(StoryRouter());
 addDecorator(checkA11y);
 
 // automatically import all files ending in *.stories.js
