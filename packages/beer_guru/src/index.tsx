18f99007831810eb9388fa8d928ca18dcc6f4d1a
Paweł Lula <pavloblack@hotmail.com>
2019-08-08 10:52:30 +0200

    Start migration to typescript

--- packages/beer_guru/src/index.tsx
+++ packages/beer_guru/src/index.tsx
@@ -1,8 +1,8 @@
 import "./polyfills";
 import "./index.css";
 
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 import { App } from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 import { initConfig } from "./init";
